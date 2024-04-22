59cffd7d4aac0b5419cb472d3ed708e05e565b10
Elias-EEDAT23 <167003703+Elias-EEDAT23@users.noreply.github.com>
2024-04-22 21:23:00 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,24 @@
 const menubutton = document.querySelector('.hamburgur')
 const navibar = document.querySelector('.navbar')
+const sunicon = document.querySelector('.sun-icon')
+const moonicon = document.querySelector('.moon-icon')
 
 menubutton.addEventListener('click',()=> {
     navibar.classList.toggle('responsive');
+});
+
+sunicon. addEventListener('click',()=> {
+    document.body.classList.remove('dark-mode');
+    navibar.classList.remove('dark-mode');
+    document.querySelector('.footer').classList.remove('dark-mode');
+    sunicon.classList.add('active');
+    moonicon.classList.remove('active');
+});
+
+moonicon. addEventListener('click',()=> {
+    document.body.classList.add('dark-mode');
+    navibar.classList.add('dark-mode');
+    document.querySelector('.footer').classList.add('dark-mode');
+    sunicon.classList.remove('active');
+    moonicon.classList.add('active');
 });
