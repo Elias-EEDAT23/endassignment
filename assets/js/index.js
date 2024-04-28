93a1bcd8511ff076d773de8e65b6da11ee49f115
Elias-EEDAT23 <167003703+Elias-EEDAT23@users.noreply.github.com>
2024-04-28 16:29:16 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,16 @@
+// kod för hamburgaren 
 const menubutton = document.querySelector('.hamburgur')
-const navibar = document.querySelector('.navbar')
-const sunicon = document.querySelector('.sun-icon')
-const moonicon = document.querySelector('.moon-icon')
 
 menubutton.addEventListener('click',()=> {
     navibar.classList.toggle('responsive');
 });
 
+// kod för light dark switch 
+const navibar = document.querySelector('.navbar')
+const sunicon = document.querySelector('.sun-icon')
+const moonicon = document.querySelector('.moon-icon')
+
+//om man klickar på solen så tas det mörka bort från meny, footer, body och solen blir aktiv
 sunicon. addEventListener('click',()=> {
     document.body.classList.remove('dark-mode');
     navibar.classList.remove('dark-mode');
@@ -15,6 +19,7 @@ sunicon. addEventListener('click',()=> {
     moonicon.classList.remove('active');
 });
 
+//om man klickar på solen så tas det mörka bort från meny, footer, body och solen blir aktiv
 moonicon. addEventListener('click',()=> {
     document.body.classList.add('dark-mode');
     navibar.classList.add('dark-mode');
@@ -22,3 +27,48 @@ moonicon. addEventListener('click',()=> {
     sunicon.classList.remove('active');
     moonicon.classList.add('active');
 });
+
+// jag lyckades inte få dark / light mode att fastna mellan sidorna
+
+
+
+// kod för bildkarusellen
+
+let imgindex=1;
+showimg(imgindex);
+
+function currentimg(n) {
+    showimg(imgindex = n);
+}
+
+
+// Funktion för bilder och indikatorpunkter
+function showimg(n) {
+    
+    let i; // bildräknare
+    let img = document.getElementsByClassName("imgs"); //hämta alla bilder
+    let dots = document.getElementsByClassName("dot"); //hämta alla punkter
+
+    if (n > img.length) {
+        imgindex = 1;
+    }
+
+
+    if (n < 1) {
+        imgindex = img.length;
+    }
+
+
+    for (i = 0; i < img.length; i++) {
+        img[i].style.display="none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    img[imgindex - 1].style.display = "block"; //visa rätt bild
+    dots[imgindex - 1].className += " active"; //marker rätt punkt
+
+
+}
